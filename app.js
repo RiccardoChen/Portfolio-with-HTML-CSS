3ee7e7c5d52c8673841794c664c902247dac7347
Riccardo Chen <chenriccardo666@gmail.com>
2026-02-05 18:36:22 +0100

    add new projects

---
+++ app.js
@@ -0,0 +1,11 @@
+const projectBtn = document.querySelector("#pBtn");
+const contactBtn = document.querySelector("#cBtn");
+
+projectBtn.addEventListener("click", () => {
+  window.location.href = "https://github.com/RiccardoChen";
+});
+
+contactBtn.addEventListener("click", () => {
+  window.location.href =
+    "https://www.linkedin.com/in/riccardo-chen-chenyilun666";
+});
